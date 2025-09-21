cb01b695036e5c19f842f9a3166cdcb08355cc85
akshatkrn07-ai <akshatkrn07@gmail.com>
2025-09-21 21:32:49 +0530

    TEST1

---
+++ lab1 Q1.py
@@ -0,0 +1,12 @@
+name = "akshat"
+print("my name is", name)
+print(f"my name is {name}")
+
+sc = int(input("eNTER yOUR nAME: "))\
+
+if sc == 2:
+    print("yo")
+elif sc == 0:
+    print("no")
+else:
+    print()
